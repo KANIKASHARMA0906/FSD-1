75dcb00695d61870a241f12dd4b0473b6a833e23
KANIKA SHARMA <130988994+KANIKASHARMA0906@users.noreply.github.com>
2025-02-10 09:51:40 +0530

    Delete FileAsync directory

--- FileAsync/index.js
+++
@@ -1,7 +0,0 @@
-const fileOps = require('./myFileData'); // Import functions
-
-fileOps.myWriteFile();  // Writing to file
-fileOps.myReadFile();   // Reading file content
-fileOps.myAppendFile(); // Appending data
-fileOps.myReadFile();   // Reading after append
-fileOps.myDeleteFile(); // Deleting file
