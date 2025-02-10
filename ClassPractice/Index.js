cc37e104e0b9beb0b7d83dbe5e3426dff792da1c
KANIKA SHARMA <130988994+KANIKASHARMA0906@users.noreply.github.com>
2025-02-10 09:51:02 +0530

    Delete ClassPractice directory

--- ClassPractice/Index.js
+++
@@ -1,4 +0,0 @@
-import { avg, avgsqrt } from './Utils.js'; // Use ES Module import
-
-console.log("Average:", avg(4, 9, 16)); 
-console.log("Average of Square Roots:", avgsqrt(4, 9, 16));
--- ClassPractice/Utils.js
+++
@@ -1,7 +0,0 @@
-export function avg(n1, n2, n3) {
-    return (n1 + n2 + n3) / 3;
-}
-
-export function avgsqrt(n1, n2, n3) {
-    return Math.sqrt(avg(n1, n2, n3)); // Use avg function inside avgsqrt
-}
